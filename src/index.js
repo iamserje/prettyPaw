af45f0c9e5a119bc7d496639ca262401da56b280
iamserje <iamserje@gmail.com>
2023-05-06 02:42:24 +0300

    feat: do about slidr, begin career section

--- src/index.js
+++ src/index.js
@@ -3,9 +3,36 @@ import './index.html';
 import 'swiper/scss';
 import 'swiper/scss/pagination';
 import './index.scss';
+import { slidersInit } from './modules/sliders';
 
 
 // ese here
+
+slidersInit('.about__slider', {
+   pagination: {
+      el: '.about__slider-pagination'
+   }
+});
+
+// slidersInit('.about__slider', {
+//    pagination: {
+//       el: '.about__slider-pagination'
+//    },
+//    breakpoints: {
+//       768: {
+//          slidersPerView: 2,
+//          spaceBetween: 2
+//       },
+//       1024: {
+//          slidersPerView: 3,
+//          spaceBetween: 3
+//       },
+//       1240: {
+//          slidersPerView: 4,
+//          spaceBetween: 4
+//       }
+//    }
+// });
 
 const videoBg = document.querySelector('.video-bg');
 videoBg.innerHTML = `
