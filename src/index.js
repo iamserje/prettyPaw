c52703cd34508fb16a51e1228e900c9e6b5ee668
iamserje <iamserje@gmail.com>
2023-05-09 09:38:41 +0300

    feat: add video in location

--- src/index.js
+++ src/index.js
@@ -37,8 +37,12 @@ slidersInit('.career__slider', {
    }
 });
 
-const videoBg = document.querySelector('.video-bg');
-videoBg.innerHTML = `
+
+const videoBg = document.querySelectorAll('.video-bg');
+
+videoBg.forEach(block => {
+   block.innerHTML = `
    <source src="video/nice-nature.webm" type="video/webm">
    <source src="video/nice-nature.mp4" type="video/mp4">
 `;
+});
